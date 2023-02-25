4cc0cd0415c2c8173089ea979acf099ddec7f3d6
tarikochuery <ttarikechuery@gmail.com>
2023-02-25 15:32:49 -0300

    Fix: Fixed middleware not working

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export const validateToken = (req, res, next) => {
-  const token = req.body.token.replace('Bearer ', '');
-  jwt.verify(token, proccess.env.SECRET, (err, decoded) => {
+  const token = req.headers.authorization.replace('Bearer ', '');
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) return res.status(401).send('Invalid Token');
 
     res.locals.userId = decoded.userId;
+    next();
   });
 };
