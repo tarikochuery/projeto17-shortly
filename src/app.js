a20767fdd83d9c542413ac8eb8e931278b70ca16
tarikochuery <ttarikechuery@gmail.com>
2023-02-25 07:15:12 -0300

    add bcrypt lib, add authRouter in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { authRouter } from './routers/authRouter.js';
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.use(authRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
