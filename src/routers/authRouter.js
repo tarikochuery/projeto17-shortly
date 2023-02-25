da49df76a9184dc8bed85c7a272bc3b4648c6a65
tarikochuery <ttarikechuery@gmail.com>
2023-02-25 07:22:55 -0300

    Fix: add body validation to POST /signup

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authControllers from "../controllers/authControllers.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import authSchemas from "../schemas/authSchemas.js";
 
 export const authRouter = Router();
 
-authRouter.post('/signup', authControllers.signup);
+authRouter.post('/signup', validateSchema(authSchemas.signUpSchema), authControllers.signup);
